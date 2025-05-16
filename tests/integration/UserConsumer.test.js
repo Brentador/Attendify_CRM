d9011f81ee8a36e64860f95d9cb5dfbc2b30af7c
Brent <145969706+Brentador@users.noreply.github.com>
2025-05-16 19:56:59 +0200

    ci/cd debugging

--- tests/integration/UserConsumer.test.js
+++ tests/integration/UserConsumer.test.js
@@ -1,58 +1,58 @@
-const amqp = require('amqplib');
-const connectRabbitmq = require('../../salesforce-service/rabbitmq');
-const { getConnection } = require('../../salesforce-service/salesforce');
+// const amqp = require('amqplib');
+// const connectRabbitmq = require('../../salesforce-service/rabbitmq');
+// const { getConnection } = require('../../salesforce-service/salesforce');
 
-describe('Consumer Tests', () => {
-    let connection, channel
+// describe('Consumer Tests', () => {
+//     let connection, channel
   
 
-    // beforeAll(async () => {
-    //     connection = await connectRabbitmq();
-    //     channel = await connection.createChannel();
-    // });
+//     // beforeAll(async () => {
+//     //     connection = await connectRabbitmq();
+//     //     channel = await connection.createChannel();
+//     // });
 
-    // afterEach(() => {
-    //     jest.clearAllMocks();
-    // });
+//     // afterEach(() => {
+//     //     jest.clearAllMocks();
+//     // });
 
-    // afterAll(async () => {
-    //     await channel.close();
-    //     await connection.close();
-    // });
+//     // afterAll(async () => {
+//     //     await channel.close();
+//     //     await connection.close();
+//     // });
 
 
-    it('should revieve a message from the rabbitMQ', async () => {
-        const xml = 
-        `<attendify>
-            <info>
-                <operation>create</operation>
-                <sender>external</sender>
-            </info>
-            <user>
-                <email>test@example.com</email>
-                <first_name>Zena</first_name>
-                <last_name>Bollaerts</last_name>
-                <title>Ms.</title>
-                <uid>SF123456789</uid>
-            </user>
-        </attendify>`;
+//     it('should revieve a message from the rabbitMQ', async () => {
+//         const xml = 
+//         `<attendify>
+//             <info>
+//                 <operation>create</operation>
+//                 <sender>external</sender>
+//             </info>
+//             <user>
+//                 <email>test@example.com</email>
+//                 <first_name>Zena</first_name>
+//                 <last_name>Bollaerts</last_name>
+//                 <title>Ms.</title>
+//                 <uid>SF123456789</uid>
+//             </user>
+//         </attendify>`;
 
-        // channel.sendToQueue('crm.user', Buffer.from(xml), { persistent: true });
+//         // channel.sendToQueue('crm.user', Buffer.from(xml), { persistent: true });
 
-        // const received = await new Promise((resolve) => {
-        //     channel.consume('crm.user', (msg) => {
-        //         resolve(msg.content.toString());
-        //         channel.ack(msg);
-        //     })
-        // })
-        // expect(received).toBe(xml);
-    })
+//         // const received = await new Promise((resolve) => {
+//         //     channel.consume('crm.user', (msg) => {
+//         //         resolve(msg.content.toString());
+//         //         channel.ack(msg);
+//         //     })
+//         // })
+//         // expect(received).toBe(xml);
+//     })
 
-    // it('should connect to salesforce', async () => {
-    //     const connection = await getConnection();
-    //     expect(connection).toBeDefined();
-    // });
+//     // it('should connect to salesforce', async () => {
+//     //     const connection = await getConnection();
+//     //     expect(connection).toBeDefined();
+//     // });
 
     
-})
+// })
 
