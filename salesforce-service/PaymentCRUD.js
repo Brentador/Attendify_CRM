60deb9a0edb12842bba7cacf2b705c229b9feeb1
Brent <145969706+Brentador@users.noreply.github.com>
2025-05-17 20:27:59 +0200

    Update PaymentCRUD.js

--- salesforce-service/PaymentCRUD.js
+++ salesforce-service/PaymentCRUD.js
@@ -12,7 +12,7 @@ class EventPaymentService {
     static async getAnotherSalesforceId(objectType, user_uid, event_uid, timestamp) {
       const conn = await getConnection();
       const result = await conn.query(`
-        SELECT Id FROM ${objectType} WHERE user_uid__c = '${user_uid}' AND event_uid__c = '${event_uid}' AND timestamp__c = ${timestamp} LIMIT 1
+        SELECT Id FROM ${objectType} WHERE user_uid__c = '${user_uid}' AND event_uid__c = '${event_uid}' AND timestamp__c = '${timestamp}' LIMIT 1
       `);
       return result.records[0]?.Id || null;
     }
