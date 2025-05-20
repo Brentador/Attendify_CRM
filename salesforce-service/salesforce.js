27c79b7a5740c8401776091c6dfa6129a4affc2a
Ibenhoe <iben.vant@gmail.com>
2025-05-20 18:40:05 +0200

    Maybe maybe good

--- salesforce-service/salesforce.js
+++ salesforce-service/salesforce.js
@@ -30,21 +30,6 @@ async function getConnection() {
   }
 }
 
-async function getEventId(eventId) {
-  try {
-    const conn = await getConnection();
-    const result = await conn.query(`SELECT Id FROM Eventcrm__c WHERE Name = '${eventId}'`);
-    if (result.records.length > 0) {
-      return result.records[0].Id; // Return the Salesforce User ID
-    } else {
-      throw new Error(`No event found with Name = ${eventId}`);
-    }
-  } catch (error) {
-    console.error('Error fetching event ID:', error);
-    throw error;
-  }
-}
-
 async function getEventId(eventUid) {
   try {
     const conn = await getConnection();
@@ -58,6 +43,23 @@ async function getEventId(eventUid) {
     }
   } catch (error) {
     console.error("Error fetching event ID:", error);
+    throw error;
+  }
+}
+
+async function getUserId(userName) {
+  try {
+    const conn = await getConnection();
+    const result = await conn.query(
+      `SELECT Id FROM Users_CRM__c WHERE Name = '${userName}'`
+    );
+    if (result.records.length > 0) {
+      return result.records[0].Id;
+    } else {
+      throw new Error(`❌ No user found with Name = '${userName}'`);
+    }
+  } catch (error) {
+    console.error('Error fetching user ID:', error);
     throw error;
   }
 }
