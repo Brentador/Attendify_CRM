3ac2a231feeda3646170cee7b459acb122f18ae1
Brent <145969706+Brentador@users.noreply.github.com>
2025-05-24 14:09:55 +0200

    Update logging.js

--- salesforce-service/logging.js
+++ salesforce-service/logging.js
@@ -1,3 +1,5 @@
+const { Builder } = require('xml2js');
+
 async function logToMonitoring(message, exchange, channel) {
     const routingKey = 'monitoring.log';
     const logMessage = {
@@ -6,7 +8,10 @@ async function logToMonitoring(message, exchange, channel) {
         message: message
     };
 
-    channel.publish(exchange, routingKey, Buffer.from(logMessage), { durable : true });
+    const builder = new Builder();
+    const logXML = builder.buildObject(logMessage);
+
+    channel.publish(exchange, routingKey, Buffer.from(logXML), { persistent : true });
 
     console.log(`Log sent to exchange ${exchange} with routing key ${routingKey}: ${message}`);
 }
